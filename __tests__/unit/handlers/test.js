5ed1cc3da4daa02926efc3ba480a8030f07d1d98
Nabby12 <Nabby12@users.noreply.github.com>
2021-01-08 02:05:33 +0900

    added/lib-for-test-code

---
+++ __tests__/unit/handlers/test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const sinon = require('sinon');
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const assert = chai.assert;
+const expect = chai.expect;
+const proxyquire = require('proxyquire');
+
+const { createSandbox } = require('sinon');
+
+describe('Test for index', function () {
+    it('test', async () => {
+        
+    });
+});
