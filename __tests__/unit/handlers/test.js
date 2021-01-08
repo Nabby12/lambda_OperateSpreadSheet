c33cb442b3423304eb4a59e446be6c79fa1d88f6
Nabby12 <Nabby12@users.noreply.github.com>
2021-01-08 09:10:32 +0900

    added/test-cases

--- __tests__/unit/handlers/test.js
+++ __tests__/unit/handlers/test.js
@@ -10,7 +10,27 @@ const proxyquire = require('proxyquire');
 const { createSandbox } = require('sinon');
 
 describe('Test for index', function () {
-    it('test', async () => {
+    it('認証実行関数でエラーが発生した場合、認証失敗ステータスが返る', async () => {
+        
+    });
+
+    it('認証処理に失敗した場合、認証失敗ステータスが返る', async () => {
+        
+    });
+
+    it('getCellsVallueが失敗した場合、APIエラーステータスが返る', async () => {
+        
+    });
+
+    it('getCellsVallueが成功した場合、成功ステータスが返る', async () => {
+        
+    });
+    
+    it('updateCellsVallueが失敗した場合、APIエラーステータスが返る', async () => {
+        
+    });
+
+    it('updateCellsVallueが成功した場合、成功ステータスが返る', async () => {
         
     });
 });
